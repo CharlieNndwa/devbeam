b19b61ec32cf06157ab4a60e165cb85ed57f4a91
unknown <you@example.com>
2026-01-05 19:16:42 +0200

    Se7eventh commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,13 @@ window.addEventListener("scroll", function () {
     goTopBtn.classList.remove("active");
   }
 });
+
+document.querySelectorAll(".pricing-card").forEach(card => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.setProperty("--x", `${x}px`);
+    card.style.setProperty("--y", `${y}px`);
+  });
+});
